8fa14b18a55f972a190e7bc8ef98f665c9f0bc06
Flavia Brogle <flavia.brogle@hotmail.com>
2022-09-17 19:33:12 +0200

    Start working on Tilemaps

---
+++ Assets/Scripts/TileRoomController.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileRoomController : MonoBehaviour {
+    private void Start() {
+        var grid = FindObjectOfType<Grid>();
+        grid.ge
+    }
+}
